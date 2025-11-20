58e0f3cd8f5832bd4af574fca05ba752c73221ab
Ayush Gupta <omthebest172@gmail.com>
2025-11-20 17:21:14 +0530

    Table Formatting update

--- struct_payslip.c
+++ struct_payslip.c
@@ -46,9 +46,9 @@ int main(){
      //net pay is the money we get in hand and total earnings is the basic pay +allowances before deductions
     //
 
-   printf("-------------------------------------------------------------------------------\n");
+   printf("---------------------------------------------------------------------------------------\n");
     printf("EMP ID\tName\t\tBasic Pay\tNet Pay\n");
-printf("-------------------------------------------------------------------------------\n");
+printf("---------------------------------------------------------------------------------------\n");
   for (i = 0; i<records; i++){
     //da = Dearness allowances, hra = house rent allowance, city = city allowance, extra is your extra allowances
     float da = 0.58 * E[i].basic_pay;
@@ -61,10 +61,10 @@ printf("------------------------------------------------------------------------
 
     float net = te - td;
 
-    printf("%-6d %-15s ₹%-12f ₹%-15f\n",E[i].id, E[i].name, E[i].basic_pay, net);}
-    printf("-------------------------------------------------------------------------------\n");
-     printf("-------------------------------------------------------------------------------\n");
-     printf("-------------------------------------------------------------------------------\n");
+    printf("%-7d %-15s ₹%-12.2f ₹%-15.2f\n",E[i].id, E[i].name, E[i].basic_pay, net);}
+    printf("---------------------------------------------------------------------------------------\n");
+    printf("---------------------------------------------------------------------------------------\n");
+    printf("---------------------------------------------------------------------------------------\n");
   
     int n2;
     
@@ -73,6 +73,7 @@ printf("------------------------------------------------------------------------
 payslip_menu:
 
     printf("Enter the EMP ID for which the Detailed Pay slip you want to check : ");
+    
     scanf("%d", &n2);
 
    //doing linear search
@@ -88,46 +89,42 @@ payslip_menu:
         printf("invalid number, please input a valid response\n");
         goto checker;
     }
-
+    
     int idx = foundIndex;
-    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("---------------------------------------------------------------------------------------\n");
+    
     float da = 0.58*E[idx].basic_pay;
     float hra =  0.16*E[idx].basic_pay;
     float callow = E[idx].cl;
     float exallow = E[idx].alow;
     float te = E[idx].basic_pay+ 0.58*E[idx].basic_pay+ 0.16*E[idx].basic_pay + E[idx].cl + E[idx].alow;
-    
-    printf("November Month For 2025 Year \n The detailed payslip of the employee is\n Name - %s \n Emp id - %d\n",E[idx].name, E[idx].id );
-    printf("Earnings\t\tAmount\n");
-    printf("-------------------------------------------------------------------------------\n");
-    
-    printf("Basic Pay\t\t₹%f\n", E[idx].basic_pay);
-    printf("Dearness Allowance\t₹%f\n", da);
-    
-    printf("House Rent Allowance\t₹%f\n",hra);
-    printf("City Allowance\t\t₹%f\n", callow);
-    printf("Extra Allowances\t₹%f\n", exallow);
-    printf("-------------------------------------------------------------------------------\n");
-    printf("Total Earnings : ₹%f\n", te);
-
-    
-    float pt = 0.13*te;
+     float pt = 0.13*te;
     float ins = 0.03*te;
     float tds =  0.05*te;
     float td = pt + ins + tds+ E[idx].ded;
-    printf("-------------------------------------------------------------------------------\n");
-    printf("Deductions\t\tAmount\n");
-    printf("-------------------------------------------------------------------------------\n");
-    printf("Professional Tax\t₹%f\n", pt);
-    printf("Insurance\t\t₹%f\n", ins);
-    printf("Tax deducted at source\t₹%f\n", tds);
-    printf("Extra deductions\t₹%f\n", E[idx].ded);
-    printf("-------------------------------------------------------------------------------\n");
-    printf("Total Deductions : ₹%f\n", td);
-    printf("-------------------------------------------------------------------------------\n");
 
-    printf("The Net pay of Employee is = ₹%f\n", te-td);
-    printf("-------------------------------------------------------------------------------\n");
+    printf("\t\t\tPayslip - November 2025 \nThe detailed payslip of the employee is\nName - %s \nEmployee ID - %d\n",E[idx].name, E[idx].id );
+    printf("---------------------------------------------------------------------------------------\n");
+    printf("Earnings\t\t  Amount\t|\tDeductions\t\tAmount\n");
+    printf("---------------------------------------------------------------------------------------\n");
+    
+    printf("Basic Pay\t\t  ₹%-7.2f\t|\tProfessional Tax\t₹%.2f\n", E[idx].basic_pay, pt);
+    printf("Dearness Allowance(DA)\t  ₹%-7.2f\t|\tInsurance\t\t₹%.2f\n", da, ins );
+    
+    printf("House Rent Allowance(HRA) ₹%-7.2f\t|\tTax deducted at source\t₹%.2f\n",hra, tds);
+    printf("City Allowance(CA)\t  ₹%-7.2f\t|\tExtra deductions\t₹%.2f\n", callow, E[idx].ded);
+    printf("Extra Allowances\t  ₹%-7.2f\t|\n", exallow );
+    printf("---------------------------------------------------------------------------------------\n");
+    printf("Total Earnings : \t  ₹%-7.2f\t|\tTotal Deductions : \t₹%.2f\n ", te, td);
+
+    
+   
+   
+    printf("---------------------------------------------------------------------------------------\n");
+
+    printf("The Net pay of Employee is = ₹%.2f\n", te-td);
+    printf("---------------------------------------------------------------------------------------\n");
 
     //we wil ask the user again and again if we want to ceck the payslip of another employe
     char again;
